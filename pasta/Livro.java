11a45aec4901801f7a96c8cafb276480074adcfd
Geilson Lucas <geilson.gears@outlook.com>
2025-09-11 17:26:56 -0300

    Update Livro.java

--- pasta/Livro.java
+++ pasta/Livro.java
@@ -9,5 +9,26 @@ package com.mycompany.exfix04;
  * @author Usuario
  */
 public class Livro {
+    private String titulo;
+    private String autor;
+    private boolean disponivel;
+    
+    public Livro(String titulo, String autor){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.disponivel = true;
+    }
+    
+    public String getTitulo(){
+        return this.titulo;
+    }
+    
+    public String getAutor(){
+        return this.autor;
+    }
+    
+    public void setDisponivel(boolean b){
+        this.disponivel = b;
+    }
     
 }
