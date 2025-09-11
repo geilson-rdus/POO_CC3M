dae36ef28bff34c298350619f5bdf7e22649a718
Geilson Lucas <geilson.gears@outlook.com>
2025-09-11 17:34:12 -0300

    Update Contato.java

--- pasta/Contato.java
+++ pasta/Contato.java
@@ -21,4 +21,17 @@ public class Contato {
     public String getNome(){
         return this.nome;
     }
+    
+    public String getTelefone(){
+        return this.telefone;
+    }
+    
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    
+    public void setTelefone(String telefone){
+        this.telefone = telefone;
+    }
+    
 }
